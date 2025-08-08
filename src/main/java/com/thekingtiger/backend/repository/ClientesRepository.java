1d24707ca5a73593f4d1363db6fdc9902fbd7028
JonathanDG <32504527+JonathanDG@users.noreply.github.com>
2025-08-07 23:02:19 -0600

    UsuarioService y UsuarioRepository Agregados

--- src/main/java/com/thekingtiger/backend/repository/ClientesRepository.java
+++ src/main/java/com/thekingtiger/backend/repository/ClientesRepository.java
@@ -4,6 +4,8 @@ import com.thekingtiger.backend.model.Clientes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.ListCrudRepository;
+
 
 @Repository
 public interface ClientesRepository extends JpaRepository<Clientes, Integer> {
