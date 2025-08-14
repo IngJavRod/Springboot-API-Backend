d00fc5b4b55c8ec41564f54b04ad1ce20cf3b3d9
JonathanDG <32504527+JonathanDG@users.noreply.github.com>
2025-08-14 15:29:48 -0600

    admin update

--- backend/src/main/java/com/thekingtiger/backend/controller/AdminController.java
+++ backend/src/main/java/com/thekingtiger/backend/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.thekingtiger.backend.controller;
 
 
+import com.thekingtiger.backend.exceptions.AdminNotFoundException;
 import com.thekingtiger.backend.model.Admin;
 import com.thekingtiger.backend.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,49 @@ public class AdminController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(adminService.addAdmin(newAdmin));
 
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<Admin> getByCorreoAdmin(@RequestParam String correoAdmin){
+        Admin adminBycorreoAdmin = adminService.findByCorreoAdmin(correoAdmin);
+        if (adminBycorreoAdmin == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity<>(adminBycorreoAdmin, HttpStatus.OK);
+    }
+
+    @GetMapping("/admin/{idAdmin}")
+    public ResponseEntity<Admin> getByIdAdmin(@PathVariable Integer idAdmin){
+        return adminService.findByIdAdmin(idAdmin)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Admin loginRequest){
+        Admin admin = adminService.findByCorreoAdmin(loginRequest.getCorreoAdmin());
+
+        if(admin == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Correo no registrado");
+
+        }
+        // Comparar contraseñas
+        if (!admin.getPasswordAdmin().equals(loginRequest.getPasswordAdmin())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Contraseña incorrecta");
+        }
+
+        return ResponseEntity.ok(admin);
+    }
+
+
+    @PutMapping("/update-admin/{idAdmin}")
+    public ResponseEntity<Admin> updateAdmin(@RequestBody Admin admin, @PathVariable Integer idAdmin){
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(adminService.updateAdmin(admin,idAdmin));
+        }catch(AdminNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
     }
 }
